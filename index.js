3435a574e4e2813058f02564104cc67212bb638b
Martin Muthaura <mmuthaura 06@gmail.com>
2025-06-30 12:23:04 +0300

    Wrap DOM update in DOMContentLoaded listener; update #text content

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// Your code goes here// index.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  // 1) Grab the <p id="text">
+  const textP = document.getElementById('text');
+  // 2) Replace its content
+  textP.textContent = "This is really cool!";
+});
